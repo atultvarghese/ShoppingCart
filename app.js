0eb3ab141b23722183901ae3caa4b2584fdbab44
Atul <atultvarghese@gmail.com>
2019-07-17 18:17:12 +0530

    sign up with passport validtion added

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var  passport = require('passport');
+var flash = require('connect-flash');
 
 
 var indexRouter = require('./routes/index');
 
 mongoose.connect('mongodb://localhost:27017/shop',{useNewUrlParser:true});
-
+require('./config/passport');
 
 var app = express();
 app.engine('.hbs', expressHbs({defaultLayout:'layout', extname: '.hbs'}));
@@ -22,6 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave:false, saveUninitialized:false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
